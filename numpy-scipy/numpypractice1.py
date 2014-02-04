58a9b5a972fb8cf37e80f4dd64687c104e1108a1
Rachel Carandang <rascarandang@gmail.com>
2014-02-04 11:29:08 -0600

    first commit

---
+++ numpy-scipy/numpypractice1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -tt
+#Practicing with basic numpy functions from the online documentation 
+
+import sys
+from numpy import *
+
+
+def main():
+  a = arange(15).reshape(3, 5)
+  print a
+
+if __name__ == '__main__':
+  main()
+
+
